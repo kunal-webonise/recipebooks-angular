4e3a639749f8d1ffce50a38c836c30e170b5c147
Vivid Triangle <kunal.bhatia.merck.com>
2017-07-10 16:18:01 +0530

    created basic skelition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list.component';
+import { RecipeUnitComponent } from './recipes/recipe-unit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeUnitComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/recipes/recipe-unit.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rb-recipe-unit',
+  templateUrl: './recipe-unit.component.html'
+})
+export class RecipeUnitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
